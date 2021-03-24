b4b5c0968534beda7ba7a1f5c3323a600a47d78a
leehyekyeong <2hyekyeong@gmail.com>
2021-03-24 23:55:45 +0900

    trend-card-link mouseover effect(⇢)

--- script.js
+++ script.js
@@ -14,3 +14,29 @@ window.onclick = function(event) {
         }
     }
 }
+
+// Jquery ver. 마우스 오버 효과가 묶여서 나타남
+// mouseover --> arrow on
+// $('.trend-card a').mouseenter(function() {
+//     $('.trend-card .arrow').text(' >');
+// });
+// // mouseleave --> arrow off
+// $('.trend-card a').mouseleave(function() {
+//     $('.trend-card .arrow').text('');
+// });
+
+// vanilaJS ver. 마우스 오버 효과가 묶여서 나타남
+// var link = document.getElementsByClassName('trend-card-link');
+// function arrowOn() {
+//     for (var i=0; i<link.length; i++) {
+//         link[i].innerHTML = "read more >";
+//     }
+// }
+
+function arrowOn(event) {
+    event.target.innerHTML = "read more ⇢";
+}
+
+function arrowOff(event) {
+    event.target.innerHTML = "read more";
+}
